78d57cd43555d30f2a0cd47f7d762181748ca85c
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-03-11 21:47:52 +0100

    Task 2.3

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function underConstruction() {
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
