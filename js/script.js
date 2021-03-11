aa41b0c9ea711a8ac0e2c19457d553661b0d8c4d
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-03-11 18:16:05 +0100

    Task 2.3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 console.log("Hi, welcome to my portfolio.");
 
-function underConstruction () {
-  alert ("Sorry, this case study is still under construction.");
+function underConstruction() {
+  alert("Sorry, this case study is still under construction.");
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
