a5970b3e18737a2b3b4604ecf33d8b87a2db7a8c
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-03-23 14:23:45 +0100

    1

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ console.log(screen.width);
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos && screen.width) {
+  if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-83px";
@@ -37,23 +37,15 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
 
-
 /*
-/// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos && screen.width < 640) {
+    document.getElementById("navbar").style.top = "0";
   } else {
-    navbar.classList.remove("sticky");
+    document.getElementById("navbar").style.top = "-83px";
   }
+  prevScrollpos = currentScrollPos;
 }
 */
